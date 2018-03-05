c0950d6e75c2db535c0bb02c99ac2dc6e3cac299
quoctuan1998 <quoctuanb6@gmail.com>
2018-03-05 15:29:41 +0700

    Complete SplashActivity

---
+++ app/src/main/java/com/example/quoctuan/lazadaapp/view/splash/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.quoctuan.lazadaapp.view.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.quoctuan.lazadaapp.R;
+import com.example.quoctuan.lazadaapp.view.splash.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                }catch (Exception e) {
+
+                }finally {
+                    Intent home = new Intent(SplashActivity.this,HomeActivity.class);
+                    startActivity(home);
+                }
+            }
+        });
+        thread.start();
+    }
+}
